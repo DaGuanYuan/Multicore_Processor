13d7507229b066d9f901dae37a76ba09240e14dd
mz588 <mz588@cornell.edu>
2021-10-19 12:34:22 -0400

    Added integration test

---
+++ sim/lab2_proc/test/ProcFL_integ_test.py
@@ -0,0 +1,22 @@
+#=========================================================================
+# ProcFL_test.py
+#=========================================================================
+
+import pytest
+import random
+
+from pymtl   import *
+from harness import *
+from lab2_proc.ProcFL import ProcFL
+
+
+import integ_test
+@pytest.mark.parametrize( "name,test", [
+  asm_test( integ_test.gen_integ_test_mz588          ) ,
+  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # Add more rows to the test case table to test more complicated
+  # scenarios.
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+])
+def test_jal( name, test, dump_vcd ):
+  run_test( ProcFL, test, dump_vcd )
