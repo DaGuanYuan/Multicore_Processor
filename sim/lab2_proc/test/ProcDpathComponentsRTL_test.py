3d0e252d4500813c01fb5dbbdc7bd8e99a6164af
mz588 <mz588@cornell.edu>
2021-10-19 20:31:13 -0400

    Added DpathComp test

--- sim/lab2_proc/test/ProcDpathComponentsRTL_test.py
+++ sim/lab2_proc/test/ProcDpathComponentsRTL_test.py
@@ -59,19 +59,110 @@ def test_alu_add( dump_vcd, test_verilog ):
     [ 0xfeeeeaa3,   0xf4650000,   0,  0xf353eaa3,   '?',      '?',       '?'      ],
   ], dump_vcd, test_verilog )
 
-#''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# Add more ALU function tests
-#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-def test_alu_cp_op0( dump_vcd, test_verilog ):
+def test_alu_sub( dump_vcd, test_verilog ):
   run_test_vector_sim( AluRTL(), [
     ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
-    [ 0x00000000,   0x00000000,  11,  0x00000000,   '?',      '?',       '?'      ],
-    [ 0x0ffaa660,   0x00012304,  11,  0x0ffaa660,   '?',      '?',       '?'      ],
-    [ 0x00132050,   0xd6620040,  11,  0x00132050,   '?',      '?',       '?'      ],
-    [ 0xfff0a440,   0x00004450,  11,  0xfff0a440,   '?',      '?',       '?'      ],
-    [ 0xfeeeeaa3,   0xf4650000,  11,  0xfeeeeaa3,   '?',      '?',       '?'      ],
+    [ 0x00000000,   0x00000000,   1,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0xffff8000,   1,  0x80008000,   '?',      '?',       '?'      ],
+    [ 0x00000000,   0x00007fff,   1,  0xffff8001,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   1,  0x7f0000ff,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   1,  0x80007fff,   '?',      '?',       '?'      ],
   ], dump_vcd, test_verilog )
+
+
+def test_alu_and( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0xffffffff,   0x0f0f0f0f,   2,  0x0f0f0f0f,   '?',      '?',       '?'      ],
+    [ 0xff00ff00,   0x0f0f0f0f,   2,  0x0f000f00,   '?',      '?',       '?'      ],
+    [ 0x0ff00ff0,   0xf0f0f0f0,   2,  0x00f000f0,   '?',      '?',       '?'      ],
+    [ 0x00ff00ff,   0x0f0f0f0f,   2,  0x000f000f,   '?',      '?',       '?'      ],
+    [ 0xf00ff00f,   0xf0f0f0f0,   2,  0xf000f000,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+
+def test_alu_or( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0xff00ff00,   0x0f0f0f0f,   3,  0xff0fff0f,   '?',      '?',       '?'      ],
+    [ 0x0ff00ff0,   0xf0f0f0f0,   3,  0xfff0fff0,   '?',      '?',       '?'      ],
+    [ 0x00ff00ff,   0x0f0f0f0f,   3,  0x0fff0fff,   '?',      '?',       '?'      ],
+    [ 0xf00ff00f,   0xf0f0f0f0,   3,  0xf0fff0ff,   '?',      '?',       '?'      ],
+    [ 0x00000f0f,   0x000000ff,   3,  0x00000fff,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+def test_alu_cp_xor( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0xff00ff00,   0x0f0f0f0f,   4,  0xf00ff00f,   '?',      '?',       '?'      ],
+    [ 0x0ff00ff0,   0xf0f0f0f0,   4,  0xff00ff00,   '?',      '?',       '?'      ],
+    [ 0x00ff00ff,   0x0f0f0f0f,   4,  0x0ff00ff0,   '?',      '?',       '?'      ],
+    [ 0xf00ff00f,   0xf0f0f0f0,   4,  0x00ff00ff,   '?',      '?',       '?'      ],
+    [ 0x00000f0f,   0x000000ff,   4,  0x00000ff0,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+def test_alu_slt( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0x00000000,   0x00007fff,   5,  0x00000001,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   5,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00007fff,   5,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0x00007fff,   5,  0x00000001,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   5,  0x00000000,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+
+def test_alu_sltu( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0x00000000,   0x00007fff,   6,  0x00000001,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   6,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00007fff,   6,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0x00007fff,   6,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   6,  0x00000001,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+def test_alu_sra( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0x00000000,   0x00007fff,   7,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   7,  0x7fffffff,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00007fff,   7,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0x00007fff,   7,  0xffffffff,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   7,  0x7fffffff,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+
+def test_alu_srl( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0x00000000,   0x00007fff,   8,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   8,  0x7fffffff,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00007fff,   8,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0x00007fff,   8,  0x00000001,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   8,  0x7fffffff,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+def test_alu_sll( dump_vcd, test_verilog ):
+  run_test_vector_sim( AluRTL(), [
+    ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+    [ 0x00000000,   0x00007fff,   9,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00ffff00,   9,  0x7fffffff,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0x00007fff,   9,  0x80000000,   '?',      '?',       '?'      ],
+    [ 0x80000000,   0x00007fff,   9,  0x00000000,   '?',      '?',       '?'      ],
+    [ 0x7fffffff,   0xffff8000,   9,  0x7fffffff,   '?',      '?',       '?'      ],
+  ], dump_vcd, test_verilog )
+
+def test_alu_cp_op0( dump_vcd, test_verilog  ):
+  run_test_vector_sim( AluRTL(), [
+      ('in0           in1           fn  out*          ops_eq*   ops_lt*  ops_ltu*'),
+      [ 0x00000000,   0x00000000,  11,  0x00000000,   '?',      '?',       '?'       ],
+      [ 0x0ffaa660,   0x00012304,  11,  0x0ffaa660,   '?',      '?',       '?'       ],
+      [ 0x00132050,   0xd6620040,  11,  0x00132050,   '?',      '?',       '?'       ],
+      [ 0xfff0a440,   0x00004450,  11,  0xfff0a440,   '?',      '?',       '?'       ],
+      [ 0xfeeeeaa3,   0xf4650000,  11,  0xfeeeeaa3,   '?',      '?',       '?'       ],
+  ], dump_vcd, test_verilog  )
 
 def test_alu_cp_op1( dump_vcd, test_verilog ):
   run_test_vector_sim( AluRTL(), [
@@ -92,6 +183,5 @@ def test_alu_fn_equality( dump_vcd, test_verilog ):
     [ 0xfff0a440,   0x00004450,  14,  0x00000000,   0,        '?',       '?'      ],
     [ 0xfeeeeaa3,   0xf4650000,  14,  0x00000000,   0,        '?',       '?'      ],
   ], dump_vcd, test_verilog )
-
 
 
